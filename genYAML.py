4e183014a8f1066a1b2253cad1803a7ba3ca467b
HH <huang_hao_zi@163.com>
2021-09-21 22:38:42 +0800

    update cron

--- genYAML.py
+++ genYAML.py
@@ -40,7 +40,7 @@ def getCourseInfo():
                 minutePure = int(hour) * 60 + int(minute) - 25 # 提前25分钟推送
                 hour, minute = int(minutePure / 60), int(minutePure % 60)
                 hour = 16 + hour if hour - 8 < 0 else hour - 8
-                cronTime = '0 {} {} * * {} *'.format(minute, hour, weekDay+1)
+                cronTime = '{} {} * * {}'.format(minute, hour, weekDay+1)
                 dataJson[code][cronTime] = timeList[i] + course
     return dataJson
 
@@ -56,7 +56,7 @@ def genWorkflows(dataJson):
                 temp = temp.replace('informationContent', information)
                 temp = temp.replace('cronTimeContent', cronTime)
                 cronTimeList = cronTime.split(' ')
-                workflowName = '{}{}{}{}'.format(classCode, cronTimeList[1], cronTimeList[2], cronTimeList[5])
+                workflowName = '{}{}{}{}'.format(classCode, cronTimeList[0], cronTimeList[1], cronTimeList[4])
                 temp = temp.replace('workflowName', workflowName)
                 with open(os.path.join('./.github/workflows', workflowName + '.yml'), 'w+', encoding='utf-8') as f:
                     f.write(temp)
